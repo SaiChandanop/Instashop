c68446630d5977cf7a478a8c94b6e79e979e08fd
klobej@gmail.com <josh@alchemy50.com>
2013-08-07 22:33:04 -0400

    button validation for create seller

--- app/Instashop/Instashop/CreateSellerViewController.m
+++ app/Instashop/Instashop/CreateSellerViewController.m
@@ -51,17 +51,17 @@
     
     self.instagramUsernameLabel.text = [InstagramUserObject getStoredUserObject].username;
     
-/*    self.nameTextField.text = @"Josh Klobe";
-
-    self.addressTextField.text = @"50 Bridge St Apt 318";
-    self.cityTextField.text = @"Brooklyn";
-    self.stateTextField.text = @"NY";
-    self.zipTextField.text = @"11201";
-    self.phoneTextField.text = @"9178374622";
-    self.emailTextField.text = @"klobej@gmail.com  ";
-    self.websiteTextField.text = @"alchemy50.com";
-    self.categoryTextField.text = @"testcat";
-  */
+    /*    self.nameTextField.text = @"Josh Klobe";
+     
+     self.addressTextField.text = @"50 Bridge St Apt 318";
+     self.cityTextField.text = @"Brooklyn";
+     self.stateTextField.text = @"NY";
+     self.zipTextField.text = @"11201";
+     self.phoneTextField.text = @"9178374622";
+     self.emailTextField.text = @"klobej@gmail.com  ";
+     self.websiteTextField.text = @"alchemy50.com";
+     self.categoryTextField.text = @"testcat";
+     */
     
     [self.submitButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
     
@@ -72,48 +72,57 @@
     
 }
 
-- (void)textFieldDidEndEditing:(UITextField *)textField
-{    
-    BOOL proceed = NO;
+
+-(BOOL)fieldsValidated
+{
+    BOOL retval = NO;
+    
     if ([self.nameTextField.text length] > 0)
         if ([self.addressTextField.text length] > 0)
             if ([self.cityTextField.text length] > 0)
-                    if ([self.stateTextField.text length] > 0)
-                        if ([self.zipTextField.text length] > 0)
-                            if ([self.emailTextField.text length] > 0)
-                                if ([self.websiteTextField.text length] > 0)
-                                    if ([self.categoryTextField.text length] > 0) 
-                                            proceed = YES;
-                                         
+                if ([self.stateTextField.text length] > 0)
+                    if ([self.zipTextField.text length] > 0)
+                        if ([self.emailTextField.text length] > 0)
+                            if ([self.websiteTextField.text length] > 0)
+                                if ([self.categoryTextField.text length] > 0)
+                                    retval = YES;
     
-    if (proceed)
+    return retval;
+}
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    if ([self fieldsValidated])
         [self.submitButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     else
         [self.submitButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
-    
-    
-    
-        
 }
-//-(void)
+
 
 -(IBAction)doneButtonHit
 {
-    NSMutableDictionary *addressDictionary = [NSMutableDictionary dictionaryWithCapacity:0];
-    [addressDictionary setObject:self.nameTextField.text forKey:@"seller_name"];
-    [addressDictionary setObject:self.addressTextField.text forKey:@"seller_address"];
-    [addressDictionary setObject:self.cityTextField.text forKey:@"seller_city"];
-    [addressDictionary setObject:self.stateTextField.text forKey:@"seller_state"];
-    [addressDictionary setObject:self.zipTextField.text forKey:@"seller_zip"];
-    [addressDictionary setObject:self.phoneTextField.text forKey:@"seller_phone"];
-    [addressDictionary setObject:self.emailTextField.text forKey:@"seller_email"];    
-    [addressDictionary setObject:self.websiteTextField.text forKey:@"seller_website"];
-    [addressDictionary setObject:self.categoryTextField.text forKey:@"seller_category"];
+    if ([self fieldsValidated])
+    {
+        NSMutableDictionary *addressDictionary = [NSMutableDictionary dictionaryWithCapacity:0];
+        [addressDictionary setObject:self.nameTextField.text forKey:@"seller_name"];
+        [addressDictionary setObject:self.addressTextField.text forKey:@"seller_address"];
+        [addressDictionary setObject:self.cityTextField.text forKey:@"seller_city"];
+        [addressDictionary setObject:self.stateTextField.text forKey:@"seller_state"];
+        [addressDictionary setObject:self.zipTextField.text forKey:@"seller_zip"];
+        [addressDictionary setObject:self.phoneTextField.text forKey:@"seller_phone"];
+        [addressDictionary setObject:self.emailTextField.text forKey:@"seller_email"];
+        [addressDictionary setObject:self.websiteTextField.text forKey:@"seller_website"];
+        [addressDictionary setObject:self.categoryTextField.text forKey:@"seller_category"];
+        
+        NSLog(@"addressDictionay: %@", addressDictionary);
+        [SellersAPIHandler makeCreateSellerRequestWithDelegate:self withInstagramUserObject:[InstagramUserObject getStoredUserObject] withSellerAddressDictionary:addressDictionary];
+    }
+    UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"Sorry"
+                                                        message:@"Please fill out all fields"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil];
+    [alertView show];
     
-    NSLog(@"addressDictionay: %@", addressDictionary);
-    [SellersAPIHandler makeCreateSellerRequestWithDelegate:self withInstagramUserObject:[InstagramUserObject getStoredUserObject] withSellerAddressDictionary:addressDictionary];
-
-    self.containerScrollView.contentSize = CGSizeMake(0, self.containerScrollView.frame.size.height * 2);
     
 }
 
@@ -136,5 +145,5 @@
     
     [self.delegate createSellerDone];
 }
-    
+
 @end
