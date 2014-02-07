82778d25b2c3f5629a492e2f228ef84be5c29538
jrb3000 <joel@alchemy50.com>
2014-02-07 11:21:59 -0500

    removing test url from amber... oops

--- app/Instashop/Instashop/AmberAPIHandler.m
+++ app/Instashop/Instashop/AmberAPIHandler.m
@@ -20,7 +20,7 @@
     NSString *originURL = @"http://www.nastygal.com/clothes-tops/soft-curve-knit-black";
     originURL = [originURL stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     
-    NSString *urlRequestString = [NSString stringWithFormat:@"https://mobile.amber.io/?public_token=6ad2af4e0e1e2fb08de9&unique_token=2388&test_mode=fake_confirm&callback_url=https://amber.io/workers/proposed_recipes/test_callback&show_tutorial=false&products=%@", originURL];
+    NSString *urlRequestString = [NSString stringWithFormat:@"https://mobile.amber.io/?public_token=6ad2af4e0e1e2fb08de9&unique_token=2388&callback_url=https://amber.io/workers/proposed_recipes/test_callback&show_tutorial=false&products=%@", originURL];
     NSMutableURLRequest *URLRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlRequestString]];
     
     AmberAPIHandler *apiHandler = [[AmberAPIHandler alloc] init];
