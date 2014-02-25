bafc61e2faf87a4599093336a582ed0789cb5305
jklobe <josh@alchemy50.com>
2014-02-25 10:13:01 -0500

    replacing some urls and tokens

--- app/Instashop/Instashop/AmberViewController.m
+++ app/Instashop/Instashop/AmberViewController.m
@@ -49,9 +49,9 @@
     
     NSString *customCSSURLString = @"http://instashop.com/test_custom.css";
     
-    NSString *amberPath = [NSString stringWithFormat:@"https://mobile.amber.io/?public_token=6ad2af4e0e1e2fb08de9&unique_token=2388&custom_css_url=%@&callback_url=https://amber.io/workers/proposed_recipes/test_callback&show_tutorial=false&products=%@", [Utils getEscapedStringFromUnescapedString:customCSSURLString], [Utils getEscapedStringFromUnescapedString:self.referenceURLString]];
+    NSString *amberPath = [NSString stringWithFormat:@"https://checkout.twotap.com/?public_token= 286b2ac01d5d6139579eb903306333&unique_token=2388&custom_css_url=%@&callback_url=https://amber.io/workers/proposed_recipes/test_callback&show_tutorial=false&products=%@", [Utils getEscapedStringFromUnescapedString:customCSSURLString], [Utils getEscapedStringFromUnescapedString:self.referenceURLString]];
         
-    //http%3A%2F%2Fwww.footlocker.com%2Fproduct%2Fmodel%3A199142%2Fsku%3A99565600%2Fnike-air-max-stutter-step-mens%2Fred%2Fwhite
+
     
     NSLog(@"amberPath: %@", amberPath);
     
@@ -59,12 +59,8 @@
     NSURLRequest *amberRequestObj = [NSURLRequest requestWithURL:amberURL];
     [self.amberWebView loadRequest:amberRequestObj];
     
-/*    UIImage *shareButtonImage = [UIImage imageNamed:@"more_button.png"];
-    UIBarButtonItem *shareButton = [[UIBarButtonItem alloc] initWithImage:shareButtonImage style:UIBarButtonItemStylePlain target:self action:@selector(openActionSheet)];
-    self.navigationItem.rightBarButtonItem = shareButton;
+
     
-  */
-   
 }
 
 
